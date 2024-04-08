9f72862d10e3b6ed3841075a9717cb55951a4f1d
Juan <juandita333333@gmail.com>
2024-04-07 19:04:28 -0500

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className='font-roboto flex justify-center items-center w-screen h-screen'>
+      <h1 className='text-3xl font-bold text-red-700'>Hello Word</h1>
+    </main>
+  )
+}
+
+export default App
