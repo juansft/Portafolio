32f24d48beaf6eb5d9c2729a68059fddd7ae9951
Juan <juandita333333@gmail.com>
2024-05-13 20:32:54 -0500

    actualizacion idiomas y avance

---
+++ src/App.1.jsx
@@ -0,0 +1,54 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <nav className="bg-gray-50 p-4">
+        <div className="flex justify-between items-center flex-wrap">
+          <div className="flex-1">
+            <input type="text" placeholder="Buscar..." className="p-2 rounded-lg border-none w-full" />
+          </div>
+          <button className="block sm:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+          <ul className="hidden sm:flex justify-end mt-4 sm:mt-0 flex-1">
+            <li className="mr-6">
+              <a href="#" className="text-black">Home</a>
+            </li>
+            <li className="mr-6">
+              <a href="#" className="text-black">Sobre Mí</a>
+            </li>
+            <li className="mr-6">
+              <a href="#" className="text-black">Habilidades</a>
+            </li>
+            <li className="mr-6">
+              <a href="#" className="text-black">Proyectos</a>
+            </li>
+            <li>
+              <a href="#" className="text-black">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="flex justify-center items-center w-full h-auto bg-gray-950 p-4">
+        <h1 className="text-3xl font-bold text-white">¡Hola! Soy Carlos, ingeniero de software. Este es mi portafolio donde encontraras inoformacion sobre mi, mis Habilidades, ademas algunos proyectos en los que he trabajado.</h1>
+      </div>
+      <main className='font-roboto flex justify-center items-center w-screen h-screen'>
+        {/* Contenido principal aquí */}
+      </main>
+      <footer className="bg-gray-700 p-4 text-center text-white">
+        <a href="#" className="text-white">Contacto</a>
+      </footer>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,58 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <main className='font-roboto flex justify-center items-center w-screen h-screen'>
-      <h1 className='text-3xl font-bold text-red-700'>Hello Word</h1>
-    </main>
-  )
+    <div className="flex flex-col min-h-screen">
+      <nav className="bg-slate-800 p-4 flex justify-between items-center">
+        <ul className="flex items-center justify-center">
+          <li className="mr-6">
+            <a href="#" className="text-white">{t('moreAboutme')}</a>
+          </li>
+          <li className="mr-6">
+            <a href="#" className="text-white">{t('Projects')}</a>
+          </li>
+          <li className="mr-6">
+            <a href="#" className="text-white">{t('Skills')}</a>
+          </li>
+          <li>
+            <a href="#" className="text-white">{t('contact')}</a>
+          </li>
+        </ul>
+        <div className="flex items-center">
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className="p-2 rounded-lg border-none bg-gray-100"
+          />
+          <div className="ml-4 flex">
+            <button onClick={() => changeLanguage('en')} className="m-2 p-2 bg-blue-500 text-white">English</button>
+            <button onClick={() => changeLanguage('es')} className="m-2 p-2 bg-green-500 text-white">Español</button>
+          </div>
+        </div>
+      </nav>
+      <div className="flex justify-center items-center w-full h-auto bg-slate-200 p-36">
+        <div className="flex flex-col md:flex-row"> 
+          <div className="md:w-1/2 md:pr-16">
+            <h1 className="text-2xl font-bold text-black">{t('welcomeMessage')}</h1>
+          </div>  
+          <div className="md:w-1/2 md:pl-8"> 
+            <img src="https://cdn.motor1.com/images/mgl/krl7R/s1/audi-rs6-avant-performance-in-the-alps.jpg" alt="yo" className="w-148 h-148 rounded-3xl" />
+          </div>
+        </div>
+      </div>
+      <div className="p-96 bg-gray-900 sm:bg-slate-300 flex-1"> 
+      </div>
+      <footer className="bg-slate-200 p-16 text-center text-white">
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
